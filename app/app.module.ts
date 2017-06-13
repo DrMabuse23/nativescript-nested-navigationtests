58e32db5a3ede4e00cf8c9c3e7da8b3f9a49637a
Drmabuse23 <drmabuse@posteo.de>
2017-06-13 18:18:22 +0200

    remove files

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,33 +1,118 @@
-import {BeaconScannerModule} from "./scanner/scanner.module";
-import {DevicesModule} from "./devices/devices.module";
-import {SecurityModule} from "./security/security.module";
-import {CoreModule} from "./core/core.module";
-import { SharedModule } from "./shared/shared.module";
-import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
-import { NativeScriptModule } from "nativescript-angular/nativescript.module";
-import { AppRoutingModule } from "./app.routing";
-import { AppComponent } from "./app.component";
+import { AppComponent } from './app.component';
+import { AppRoutingModule } from './app.routing';
+import { BeaconScannerComponent } from './scanner/components/beaconscanner/beaconscanner.component';
+import { MenuOutletComponent } from './shared/components/menuoutlet/menuoutlet.component';
+import { FontIconPipe } from './shared/pipes/font-icon.pipe';
+import { HttpLambdaService } from './core/http-lambda.service';
+import { EnvironmentService } from './core/environment.service';
+import { FontService } from './core/font.service';
+import { IsEmailDirective } from './core/validators.directive';
+import { MinLengthDirective } from './core/validators.directive';
+import { ApplicationBarHelper } from './core/applicationbar.service';
+import { FabBtnComponent } from './shared/components/fab-button.component';
+import { MeterPipe } from './shared/pipes/meter.pipe';
+import { TKIfAndroidDirective } from './shared/directives/platform.directives';
+import { TKIfIOSDirective } from './shared/directives/platform.directives';
+import { DeviceComponent } from './devices/components/device/device.component';
+import { DevicesComponent } from './devices/components/devices/devices.component';
+import { LoginComponent } from './security/components/login/login-form.component';
+import { AuthGuard } from './security/services/auth-guard';
+import { UserService } from './security/services/user.service';
+
+
+import { ErrorHandler } from '@angular/core';
+import { NativeScriptRouterModule } from 'nativescript-angular/router';
+import { NativeScriptUISideDrawerModule } from 'nativescript-telerik-ui/sidedrawer/angular';
+
+import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
+import { NativeScriptFormsModule } from 'nativescript-angular/forms';
+import { NgModule } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { NativeScriptUIListViewModule } from "nativescript-telerik-ui/listview/angular";
+import { NgStringPipesModule } from 'angular-pipes';
+import { Http, RequestOptions, XHRBackend, HttpModule } from '@angular/http';
+import { NativeScriptHttpModule } from "nativescript-angular/http";
 import 'rxjs/Rx';
 
+
+class MyErrorHandler implements ErrorHandler {
+  handleError(err: any): void {
+    console.log(`Ups Oh MyErrorHandler say: ${err.message ? err.message : ''}`);
+    return err;
+  }
+}
+export function lambda(
+  backend: XHRBackend,
+  defaultOptions: RequestOptions
+) {
+  return new HttpLambdaService(backend, defaultOptions);
+}
+
+const components = [
+  AppComponent,
+  FabBtnComponent,
+  MenuOutletComponent,
+  LoginComponent,
+  DevicesComponent,
+  DeviceComponent,
+  BeaconScannerComponent
+];
+
+const directives = [
+  TKIfAndroidDirective,
+  TKIfIOSDirective,
+  MinLengthDirective,
+  IsEmailDirective
+];
+
+const pipes = [
+  MeterPipe,
+  FontIconPipe
+];
+
+const providers = [
+  { provide: ErrorHandler, useClass: MyErrorHandler },
+  EnvironmentService,
+  FontService,
+  ApplicationBarHelper,
+  UserService,
+  AuthGuard
+];
+
 @NgModule({
-    bootstrap: [
-        AppComponent
-    ],
-    imports: [
-        NativeScriptModule,
-        AppRoutingModule,
-        CoreModule.forRoot(),
-        SharedModule,
-        SecurityModule,
-        DevicesModule,
-        BeaconScannerModule
-    ],
-    declarations: [
-        AppComponent
-    ],
-    providers: [ ],
-    schemas: [
-        NO_ERRORS_SCHEMA
-    ]
+  bootstrap: [
+    AppComponent
+  ],
+  imports: [
+    HttpModule,
+    AppRoutingModule,
+    NgStringPipesModule,
+    NativeScriptUIListViewModule,
+    CommonModule,
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptRouterModule,
+    NativeScriptUISideDrawerModule
+  ],
+  declarations: [
+    ...components,
+    ...pipes,
+    ...directives
+  ],
+  exports: [
+    NgStringPipesModule,
+    NativeScriptUIListViewModule,
+    CommonModule,
+    NativeScriptModule,
+    NativeScriptFormsModule,
+    NativeScriptRouterModule,
+    NativeScriptUISideDrawerModule,
+    ...components,
+    ...pipes,
+    ...directives
+  ],
+  providers: [
+    ...providers
+  ]
 })
 export class AppModule { }
--- app/security/security.routing.ts
+++
@@ -1,6 +0,0 @@
-import { LoginComponent } from './components/login/login-form.component';
-import { Routes } from '@angular/router';
-
-export const securityRoutes: Routes = [
-  { path: "login", component: LoginComponent }
-];
