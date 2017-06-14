7f82a9716aa73de8bda2478e8cbe23d76fb9b36d
Drmabuse23 <drmabuse@posteo.de>
2017-06-14 12:30:25 +0200

    fixing shit

--- app/app.module.ts
+++ app/app.module.ts
@@ -1,118 +1,56 @@
-import { AppComponent } from './app.component';
-import { AppRoutingModule } from './app.routing';
-import { BeaconScannerComponent } from './scanner/components/beaconscanner/beaconscanner.component';
-import { MenuOutletComponent } from './shared/components/menuoutlet/menuoutlet.component';
-import { FontIconPipe } from './shared/pipes/font-icon.pipe';
-import { HttpLambdaService } from './core/http-lambda.service';
-import { EnvironmentService } from './core/environment.service';
-import { FontService } from './core/font.service';
-import { IsEmailDirective } from './core/validators.directive';
-import { MinLengthDirective } from './core/validators.directive';
-import { ApplicationBarHelper } from './core/applicationbar.service';
-import { FabBtnComponent } from './shared/components/fab-button.component';
-import { MeterPipe } from './shared/pipes/meter.pipe';
-import { TKIfAndroidDirective } from './shared/directives/platform.directives';
-import { TKIfIOSDirective } from './shared/directives/platform.directives';
-import { DeviceComponent } from './devices/components/device/device.component';
-import { DevicesComponent } from './devices/components/devices/devices.component';
-import { LoginComponent } from './security/components/login/login-form.component';
-import { AuthGuard } from './security/services/auth-guard';
-import { UserService } from './security/services/user.service';
-
-
-import { ErrorHandler } from '@angular/core';
-import { NativeScriptRouterModule } from 'nativescript-angular/router';
-import { NativeScriptUISideDrawerModule } from 'nativescript-telerik-ui/sidedrawer/angular';
-
-import { NativeScriptModule } from 'nativescript-angular/nativescript.module';
-import { NativeScriptFormsModule } from 'nativescript-angular/forms';
-import { NgModule } from '@angular/core';
-import { CommonModule } from '@angular/common';
-import { NativeScriptUIListViewModule } from "nativescript-telerik-ui/listview/angular";
-import { NgStringPipesModule } from 'angular-pipes';
-import { Http, RequestOptions, XHRBackend, HttpModule } from '@angular/http';
+import {WelcomeLoginComponent} from "./app-components/welcome-login/welcome.component";
+import { MainActionBarComponent } from "./common-components/actionbar.component";
+import { DrawerService } from "./services/drawer.service";
+import { NativeScriptFormsModule } from "nativescript-angular/forms";
 import { NativeScriptHttpModule } from "nativescript-angular/http";
-import 'rxjs/Rx';
+import { NativeScriptRouterModule } from "nativescript-angular/router";
+import { NativeScriptUISideDrawerModule } from 'nativescript-telerik-ui/sidedrawer/angular';
+import { LoginComponent } from "./app-components/login/login.component";
+import { HelpComponent } from "./app-components/help/help.component";
+import { HomeComponent } from "./app-components/home/home.component";
+import { WelcomeComponent } from "./app-components/welcome/welcome.component";
+import { NgModule, NO_ERRORS_SCHEMA } from "@angular/core";
+import { NativeScriptModule } from "nativescript-angular/nativescript.module";
+import { AppRoutingModule } from "./app.routing";
+import { AppComponent } from "./app.component";
 
+import { ItemService } from "./item/item.service";
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
 
-class MyErrorHandler implements ErrorHandler {
-  handleError(err: any): void {
-    console.log(`Ups Oh MyErrorHandler say: ${err.message ? err.message : ''}`);
-    return err;
-  }
-}
-export function lambda(
-  backend: XHRBackend,
-  defaultOptions: RequestOptions
-) {
-  return new HttpLambdaService(backend, defaultOptions);
-}
-
-const components = [
-  AppComponent,
-  FabBtnComponent,
-  MenuOutletComponent,
-  LoginComponent,
-  DevicesComponent,
-  DeviceComponent,
-  BeaconScannerComponent
-];
-
-const directives = [
-  TKIfAndroidDirective,
-  TKIfIOSDirective,
-  MinLengthDirective,
-  IsEmailDirective
-];
-
-const pipes = [
-  MeterPipe,
-  FontIconPipe
-];
-
-const providers = [
-  { provide: ErrorHandler, useClass: MyErrorHandler },
-  EnvironmentService,
-  FontService,
-  ApplicationBarHelper,
-  UserService,
-  AuthGuard
+export const APP_COMPONENTS: any[] = [
+    WelcomeComponent,
+    AppComponent,
+    HomeComponent,
+    HelpComponent,
+    LoginComponent,
+    ItemsComponent,
+    ItemDetailComponent,
+    MainActionBarComponent,
+    WelcomeLoginComponent
 ];
 
 @NgModule({
-  bootstrap: [
-    AppComponent
-  ],
-  imports: [
-    HttpModule,
-    AppRoutingModule,
-    NgStringPipesModule,
-    NativeScriptUIListViewModule,
-    CommonModule,
-    NativeScriptModule,
-    NativeScriptFormsModule,
-    NativeScriptRouterModule,
-    NativeScriptUISideDrawerModule
-  ],
-  declarations: [
-    ...components,
-    ...pipes,
-    ...directives
-  ],
-  exports: [
-    NgStringPipesModule,
-    NativeScriptUIListViewModule,
-    CommonModule,
-    NativeScriptModule,
-    NativeScriptFormsModule,
-    NativeScriptRouterModule,
-    NativeScriptUISideDrawerModule,
-    ...components,
-    ...pipes,
-    ...directives
-  ],
-  providers: [
-    ...providers
-  ]
+    bootstrap: [
+        AppComponent
+    ],
+    imports: [
+        NativeScriptModule,
+        NativeScriptModule,
+        NativeScriptFormsModule,
+        NativeScriptHttpModule,
+        NativeScriptUISideDrawerModule,
+        AppRoutingModule
+    ],
+    declarations: [
+        ...APP_COMPONENTS
+    ],
+    providers: [
+        ItemService,
+        DrawerService
+    ],
+    schemas: [
+        NO_ERRORS_SCHEMA
+    ]
 })
 export class AppModule { }
--- app/app.routing.ts
+++ app/app.routing.ts
@@ -1,42 +1,41 @@
-import {BeaconScannerComponent} from "./scanner/components/beaconscanner/beaconscanner.component";
-import {DeviceComponent} from "./devices/components/device/device.component";
-import { DevicesComponent } from "./devices/components/devices/devices.component";
-import { MenuOutletComponent } from "./shared/components/menuoutlet/menuoutlet.component";
-import { LoginComponent } from "./security/components/login/login-form.component";
+import {WelcomeLoginComponent} from "./app-components/welcome-login/welcome.component";
+import {HelpComponent} from "./app-components/help/help.component";
+import {LoginComponent} from "./app-components/login/login.component";
+import {WelcomeComponent} from "./app-components/welcome/welcome.component";
+import {HomeComponent} from "./app-components/home/home.component";
 import { NgModule } from "@angular/core";
 import { NativeScriptRouterModule } from "nativescript-angular/router";
 import { Routes } from "@angular/router";
 
+import { ItemsComponent } from "./item/items.component";
+import { ItemDetailComponent } from "./item/item-detail.component";
 
 const routes: Routes = [
-  { path: "", redirectTo: "/login", pathMatch: "full" },
-  { path: "login", component: LoginComponent },
-  {
-    path: "home",
-    component: MenuOutletComponent,
-    children: [
-      { path: "", component: DevicesComponent },
-      { 
-        path: "devices", 
-        component: DevicesComponent,
+     {
+        path: '',
+        redirectTo: '/login',
+        pathMatch: 'full'
+    },
+    { 
+        path: "login", 
+        component: LoginComponent
+    },
+    {
+        path: "home",
+        component: HomeComponent,
         children: [
-          { path: "", component: DevicesComponent },
-          { path: "device/:deviceId", component: DeviceComponent },
+            { path: '', component: WelcomeComponent },
+            
+            { path: 'loggedIn', component: WelcomeLoginComponent },
+            { path: "help", component: HelpComponent },
         ]
-      },
-      { 
-        path: "scanner", 
-        component: BeaconScannerComponent
-      },
-    ]
-  },
-
+    },
+    { path: "items", component: ItemsComponent },
+    { path: "item/:id", component: ItemDetailComponent },
 ];
 
 @NgModule({
-  imports: [
-    NativeScriptRouterModule.forRoot(routes)
-  ],
-  exports: [NativeScriptRouterModule]
+    imports: [NativeScriptRouterModule.forRoot(routes)],
+    exports: [NativeScriptRouterModule]
 })
 export class AppRoutingModule { }
